b7a108211d56eb8998ad71d4e6a9fd8a19cab8f6
Gazdag Imre <gazdag@geox.hu>
2017-11-14 09:31:25 +0100

    Initial commit

--- AntaresMapsDemo/ViewController.swift
+++ AntaresMapsDemo/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import AntaresMaps
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    var mapView: AMMapView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let camera = AMCameraPosition.camera(target: CLLocationCoordinate2D(latitude: 46.9536996, longitude: 17.8947033), zoom: 15.6)
+        mapView = AMMapView.map(frame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        let marker: AMMarker = AMMarker(position: CLLocationCoordinate2D(latitude: 46.9536996, longitude: 17.8947033))
+        marker.map = mapView
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
---
+++ Podfile
@@ -0,0 +1,26 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+source 'https://github.com/antaresnav/antares-maps-specs'
+source 'https://github.com/CocoaPods/Specs'
+
+target 'AntaresMapsDemo' do
+
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AntaresMapsDemo
+
+  pod 'AntaresMaps', '~> 0.0.2'
+
+  target 'AntaresMapsDemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'AntaresMapsDemoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
